6bc58b3ef2ad862b1f62190b9a04f0f590092c9b
ANaRobe <ana.maria.robe@gmail.com>
2023-01-18 12:38:28 +0000

    add alert for in case passwords don't match

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -33,7 +33,7 @@ const SignUpForm = () => {
         try {
           await axios.post("/dj-rest-auth/registration/", Data);
         } catch (error) {
-            setErrors(error.response?.data);  //
+            setErrors(error.response?.data);  // Check if response is defined before looking at the data
           }
       };
     return (
@@ -42,29 +42,31 @@ const SignUpForm = () => {
     <Form onSubmit={handleSubmit}>
         {errors.username?.map((msg, idx) => (
                 <Alert variant="warning" key={idx}>{msg}</Alert>
-                ))}
+              ))}
         <Form.Group controlId="username">
             <Form.Label>Username</Form.Label>
             <Form.Control type="text" placeholder="username" name="username" value={username} onChange={handleChange}/>
         </Form.Group>
         {errors.password1?.map((msg, idx) => (
                 <Alert variant="warning" key={idx}>{msg}</Alert>
-                ))}
+              ))}
         <Form.Group controlId="password1">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" name="password1" value={password1} onChange={handleChange}/>
         </Form.Group>
         {errors.password2?.map((msg, idx) => (
                 <Alert variant="warning" key={idx}>{msg}</Alert>
-                ))}
+              ))}
         <Form.Group controlId="password2">
             <Form.Label>Confirm password</Form.Label>
             <Form.Control type="password" placeholder="Confirm password" name="password2" value={password2} onChange={handleChange}/>
-        </Form.Group>
-
+        </Form.Group> 
         <Button variant="primary" type="submit">
             Sign up
         </Button>
+        {errors.non_field_errors?.map((msg, idx) => (
+                <Alert variant="warning" key={idx}>{msg}</Alert>
+              ))}
     </Form>
     </>
   );
