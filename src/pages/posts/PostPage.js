62684210ded55b80a6a6980613992c210098faa9
ANaRobe <ana.maria.robe@gmail.com>
2023-01-23 14:51:59 +0000

    CreateCommentForm and Comment component

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -2,19 +2,29 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import CreateCommentForm from "../comments/CreateCommentForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Container from "react-bootstrap/Container";
+import appStyles from "../../App.module.css";
+import Comment from "../comments/Comment";
 
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
-    console.log(post);
+  
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
+
   useEffect(() => {
     const onMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (error) {
         console.log(error);
       }
@@ -24,7 +34,31 @@ function PostPage() {
   }, [id]);
 
   return (
-   <Post {...post.results[0]} setPosts={setPost} postPage />
+    <>  
+    <Post {...post.results[0]} setPosts={setPost} postPage />
+    <Container className={appStyles.Content}>
+    {currentUser ? (
+      <CreateCommentForm
+        profile_id={currentUser.profile_id}
+        profileImage={profile_image}
+        post={id}
+        setPost={setPost}
+        setComments={setComments}
+      />
+    ) : comments.results.length ? (
+      "Comments"
+    ) : null}
+              {comments.results.length ? (
+            comments.results.map((comment) => (
+              <Comment key={comment.id} {...comment} />
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
+  </Container></>
+   
   );
 }
 
